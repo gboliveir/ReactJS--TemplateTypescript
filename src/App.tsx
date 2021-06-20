4aed462bd56458eb0a2a8f378113338196ebff3d
Gabriel Brito Oliveira <gabrielbrito.dev@gmail.com>
2021-06-20 13:43:25 -0300

    Adocionando configuração de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import Routes from './routes';
 import './styles/global.css'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hellow World</h1>
-    </div>
+    <Routes />
   );
 }
 
